9985a748ebfcc1058903145a4d54255c58c5b727
PiotrSzcz <piotr.szczepanski.ico@gmail.com>
2023-05-25 18:48:28 +0200

    initial commit

---
+++ src/Parser.java
@@ -0,0 +1,15 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+
+import java.io.IOException;
+
+public class Parser {
+
+    public static void main(String[] args) throws IOException {
+
+        CharStream charStream = CharStreams.fromFileName("./exaple.cc");
+
+    }
+
+
+}
